cc04003811ba180158498b116ba04b5a861e4297
jasonj <617229004@qq.com>
2023-03-27 23:05:26 +0800

    更新converter 为java class

--- lightning-boot-starters/lightning-boot-starter-generic-crud-service/src/main/java/com/jianyue/lightning/boot/starter/generic/crud/service/support/converters/Converter.java
+++ lightning-boot-starters/lightning-boot-starter-generic-crud-service/src/main/java/com/jianyue/lightning/boot/starter/generic/crud/service/support/converters/Converter.java
@@ -9,14 +9,16 @@ public interface Converter<S,T> extends ConverterSupport {
     boolean support(Object param);
 
 
-    /**
-     * 由于 kotlin的使用点 差异,导致有可能拿到的converter 是一个S == Noting的情况(例如使用 out(生产者的情况下),只能生产不能消费)
-     * 借助此方法实现,一个伪强转 ..
-     */
-    @SuppressWarnings("unchecked")
-    default T convertForAny(Object param) {
-        return convert(((S) param));
-    }
+//    /**
+//     * 由于 kotlin的使用点 差异,导致有可能拿到的converter 是一个S == Noting的情况(例如使用 out(生产者的情况下),只能生产不能消费)
+//     * 借助此方法实现,一个伪强转 ..
+//     *
+//     * java 好像不再需要 ..
+//     */
+//    @SuppressWarnings("unchecked")
+//    default T convertForAny(Object param) {
+//        return convert(((S) param));
+//    }
 
 
     /**
