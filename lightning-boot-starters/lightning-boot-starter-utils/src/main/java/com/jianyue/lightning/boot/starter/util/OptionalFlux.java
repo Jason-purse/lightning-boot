18f9a446b67d27674ca85f3a3adcf5a8ccacac34
jasonj <617229004@qq.com>
2023-04-06 23:20:32 +0800

    optionalflux function flush

--- lightning-boot-starters/lightning-boot-starter-utils/src/main/java/com/jianyue/lightning/boot/starter/util/OptionalFlux.java
+++ lightning-boot-starters/lightning-boot-starter-utils/src/main/java/com/jianyue/lightning/boot/starter/util/OptionalFlux.java
@@ -6,9 +6,10 @@ import org.jetbrains.annotations.Nullable;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.ResolvableType;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.function.*;
 
 /**
@@ -443,109 +444,5 @@ public class OptionalFlux<S> {
     @Override
     public int hashCode() {
         return Objects.hash(this, value);
-    }
-
-    /**
-     * TODO
-     * <p>
-     * <p>
-     * 有一个需求,将任何类型转换为这个TypeRefer所指定的类型,前提是开发者知道这个已知类型,仅仅是将它转换为具体类型 ..
-     * 例如 Object -> List<String>
-     * Object -> Map<String,List<Map>
-     * <p>
-     * 如何做到这样的事情呢 ???
-     *
-     * @param <T>
-     * @deprecated
-     */
-    public abstract static class TypeRefer<T> {
-
-        public Type getGenericType() {
-            Type type = this.getClass().getGenericSuperclass();
-            ParameterizedType parameterizedType = (ParameterizedType) type;
-            return parameterizedType.getActualTypeArguments()[0];
-        }
-    }
-
-    /**
-     * 检查类型是否匹配 ...(不做隐式转换)
-     * <p>
-     * 例如 Set 属于 Collection ...(仅仅强类型比较)
-     * 不考虑 协变
-     *
-     * @param source    source object
-     * @param typeRefer type refer
-     * @param <T>       generic type
-     * @return T
-     * @throws ClassCastException exception ..
-     */
-    @SuppressWarnings("unchecked")
-    public static <T> T checkType(OptionalFlux<Object> source, TypeRefer<T> typeRefer) {
-
-        if (!source.getValue().isPresent()) {
-            // null
-            return (T) null;
-        }
-        final Type genericType = typeRefer.getGenericType();
-        // 直接复写 TypeRefer 的getGenericType方法 ...
-        if (genericType instanceof Class) {
-            source.getResult(((Class<T>) genericType));
-        }
-
-        // 否则参数化类型 ...
-        else if (genericType instanceof ParameterizedType) {
-            final ParameterizedType gType = (ParameterizedType) genericType;
-            final Type[] actualTypeArguments = gType.getActualTypeArguments();
-            // 尝试判断列表 ...
-            final Type actualTypeArgument = actualTypeArguments[0];
-            if (actualTypeArgument instanceof Class) {
-                source.getResult(((Class<T>) actualTypeArgument));
-            }
-
-            // 否则有可能还是一个参数化类型
-            if (actualTypeArgument instanceof ParameterizedType) {
-                ParameterizedType parameterizedType = (ParameterizedType) actualTypeArgument;
-
-                Type rawType = parameterizedType.getRawType();
-                // 这必须是一个 class
-                if (isCollection(((Class<?>) rawType))) {
-                    // 必然不为空,source
-                    assert source.getTargetClass() != null;
-                    Class<Collection<?>> collectionClass = (Class<Collection<?>>) rawType;
-                    if (collectionClass.isAssignableFrom(source.getTargetClass())) {
-                        Collection<?> cast = collectionClass.cast(source);
-                        // 如果为空,则可以转过去 ...
-                        if (cast.isEmpty()) {
-                            return (T) cast;
-                        }
-                    }
-                }
-
-//				parameterizedType
-            }
-
-
-            // 这里比较复杂 ...
-            final Class<?> aClass = source.getClass();
-            // 如果是集合比较好办,一层一层比对 ...
-            if (isCollection(aClass)) {
-//				source.getResultForList()
-            }
-
-        } else {
-            // 无法转换
-            // 也就是必须传递 class / 参数化类型,不能是类型变量或者 通配符类型
-            throw new ClassCastException("typeRefer must be one of class or parameterizedType !!!");
-        }
-        throw new ClassCastException("typeRefer must be one of class or parameterizedType !!!");
-    }
-
-    private static boolean isCollection(Class<?> clazz) {
-        return Collection.class.isAssignableFrom(clazz);
-    }
-
-
-    private static boolean isMap(Class<?> clazz) {
-        return Map.class.isAssignableFrom(clazz);
     }
 }
